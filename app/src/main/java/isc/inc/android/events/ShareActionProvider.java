08aae6a0c045186b0f2f7c4f70e930182650f590
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-09 14:32:10 +0200

    Create ShareActionProvider.java

---
+++ app/src/main/java/isc/inc/android/events/ShareActionProvider.java
@@ -0,0 +1,24 @@
+private ShareActionProvider mShareActionProvider;
+...
+
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate menu resource file.
+    getMenuInflater().inflate(R.menu.share_menu, menu);
+
+    // Locate MenuItem with ShareActionProvider
+    MenuItem item = menu.findItem(R.id.menu_item_share);
+
+    // Fetch and store ShareActionProvider
+    mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+    // Return true to display menu
+    return true;
+}
+
+// Call to update the share intent
+private void setShareIntent(Intent shareIntent) {
+    if (mShareActionProvider != null) {
+        mShareActionProvider.setShareIntent(shareIntent);
+    }
+}
