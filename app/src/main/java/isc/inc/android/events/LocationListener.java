7a51a696bf74d27a2b514ee5464e5b722a4cbf45
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-09 14:49:12 +0200

    Update LocationListener.java

--- app/src/main/java/isc/inc/android/events/LocationListener.java
+++ app/src/main/java/isc/inc/android/events/LocationListener.java
@@ -12,3 +12,23 @@ protected void startLocationUpdates() {
     LocationServices.FusedLocationApi.requestLocationUpdates(
             mGoogleApiClient, mLocationRequest, this);
 }
+
+
+// https://developer.android.com/training/location/receive-location-updates.html#callback
+
+public class MainActivity extends ActionBarActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+    ...
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    private void updateUI() {
+        mLatitudeTextView.setText(String.valueOf(mCurrentLocation.getLatitude()));
+        mLongitudeTextView.setText(String.valueOf(mCurrentLocation.getLongitude()));
+        mLastUpdateTimeTextView.setText(mLastUpdateTime);
+    }
+}
