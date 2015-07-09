3cd6bfe965c2a57819b7d48dfd1b39d22f680830
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-09 14:45:17 +0200

    Create GoogleApiClient.java

---
+++ app/src/main/java/isc/inc/android/events/GoogleApiClient.java
@@ -0,0 +1,14 @@
+// https://developer.android.com/training/location/retrieve-current.html#last-known
+
+public class MainActivity extends ActionBarActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener {
+    ...
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
