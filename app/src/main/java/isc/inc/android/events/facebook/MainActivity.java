06d86bfbfd48de243bd8425c95c5c473d3842814
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-17 16:23:41 +0200

    Create MainActivity.java

---
+++ app/src/main/java/isc/inc/android/events/facebook/MainActivity.java
@@ -0,0 +1,11 @@
+// Get Current Token
+
+public class MainActivity extends FragmentActivity {
+    CallbackManager callbackManager;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {...});
+    }
