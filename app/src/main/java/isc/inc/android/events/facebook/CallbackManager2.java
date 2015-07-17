feda086554c4b48e7f73f377f08594d3993f00b2
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-17 16:17:11 +0200

    Create CallbackManager2.java

---
+++ app/src/main/java/isc/inc/android/events/facebook/CallbackManager2.java
@@ -0,0 +1,5 @@
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    callbackManager.onActivityResult(requestCode, resultCode, data);
+}
